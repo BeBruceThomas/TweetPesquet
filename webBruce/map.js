2d96d4582afff3822ac39af3b02d8b91737f624b
BeBruceThomas <bruce.eo.thomas@gmail.com>
2017-02-05 23:59:17 +0100

    Update map.js

--- webBruce/map.js
+++ webBruce/map.js
@@ -1,5 +1,5 @@
-//var map = L.map('mapid').setView([19.421, -155.287], 13);
-var map = L.map('mapid',{zoom : 4});
+var valzoom = 4
+var map = L.map('mapid',{zoom : valzoom});
 
 // Provider de tuile OpenStreetMap
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
@@ -8,21 +8,51 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
   }
 ).addTo(map);
 
-var lat;
-var lon;
-var iss = L.marker([0,0]).addTo(map);
+var latitude;
+var longitude;
+var exlatitude = 0;
+var exlongitude = 0;
 
-function moveISS () {
+var markerLayer = L.featureGroup();
+var traceLayer = L.featureGroup();
+
+// Récupération des coordonnées en latitude et longitude de l'ISS
+function getCoords() {
     $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
-        lat = data['iss_position']['latitude'];
-        lon = data['iss_position']['longitude'];
-
-        // See leaflet docs for setting up icons and map layers
-        // The update to the map is done here:
-        iss.setLatLng([lat, lon]);
-        map.panTo([lat, lon], animate=true);
+        latitude = data['iss_position']['latitude'];
+        longitude = data['iss_position']['longitude'];
     });
-    setTimeout(moveISS, 5000);
 }
 
-moveISS();
+// Mouvement de l'ISS et dessin de la trace
+function moveISS() {
+    markerLayer.clearLayers();
+    var iss = L.marker([latitude, longitude]);
+
+    if (exlatitude!=0 && exlongitude!=0) {
+        var positions = [new L.LatLng(exlatitude, exlongitude), new L.LatLng(latitude, longitude)];
+        var trace = new L.Polyline(positions);
+        trace.addTo(traceLayer);
+    }
+
+    map.setView([latitude, longitude], 6);
+    iss.addTo(markerLayer);
+    markerLayer.addTo(map);
+    traceLayer.addTo(map);
+}
+
+// Fonction de mise à jour de la carte
+function refreshView() {
+    setTimeout(function() {
+        getCoords();
+        moveISS();
+        exlatitude = latitude;
+        exlongitude = longitude;
+        refreshView();
+    }, 1000);
+}
+
+// Initialisation de la carte avec les coordonnées de l'ISS à l'instant présent
+getCoords();
+// Mise à jour de la carte toutes les secondes
+refreshView();
