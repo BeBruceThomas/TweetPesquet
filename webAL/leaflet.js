f952bd1a1b5d7577bf4446e53cf2a8d373f1ea09
AnnelaureBeck <annelaure.beck@gmail.com>
2017-02-25 18:19:31 +0100

    Add files via upload

--- webAL/leaflet.js
+++ webAL/leaflet.js
@@ -1,56 +1,73 @@
-var valzoom=4
+var valzoom=3
 
-function coordChange(){
+function ZoomChange(){
 if(document.getElementById('smart').checked){
       valzoom=document.getElementById("smart").value;
 }else if (document.getElementById("reflex").checked) {
       valzoom=document.getElementById("reflex").value;
 }else{valzoom=document.getElementById("tele").value}
-setTimeout(coordChange, 5000);
 }
 
-coordChange();
 
-var map = L.map('map',{zoom : valzoom});
+var map = L.map('map',{zoom: valzoom});
 var osmUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © OpenStreetMap contributors';
 var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib}).addTo(map);
 
 
-var lat;
-var lon;
+var latitude;
+var longitude;
+var exlatitude = 0;
+var exlongitude = 0;
 
 var markerLayer = L.featureGroup();
 var traceLayer = L.featureGroup();
 
 
-/*ajaxGet("http://api.open-notify.org/iss-now.json", function(reponse){
-  var latitude = JSON.parse(reponse);
-  latitude.textContent = latitude.latitude;
-  var longitude = JSON.parse(reponse);
-  longitude.textContent = longitude.longitude;
-})
-
-navigator.geolocation.getCurrentPosition(function (address) {
-  map.setView([address.coords.latitude, address.coords.longitude,],13);
-  map.addLayer(osm);
-    var marker = L.marker([address.coords.latitude, address.coords.longitude,]).addTo(map);
-});*/
-
-function moveISS () {
+function getCoords() {
     $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
-        lat = data['iss_position']['latitude'];
-        lon = data['iss_position']['longitude'];
-
-        // See leaflet docs for setting up icons and map layers
-        markerLayer.clearLayers();
-        var iss = L.marker([lat, lon]).addTo(map);
-        // The update to the map is done here:
-        iss.setLatLng([lat, lon]);
-        map.panTo([lat, lon], animate=true);
-        traceLayer.addTo(map);
+        latitude = data['iss_position']['latitude'];
+        longitude = data['iss_position']['longitude'];
+        document.getElementById("latitude").textContent = latitude;
+        document.getElementById("longitude").textContent = longitude;
     });
-    setTimeout(moveISS, 5000);
 }
 
-moveISS();
+var IconISS = L.icon({
+    iconUrl: 'ISS.png',
+
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+function moveISS() {
+    markerLayer.clearLayers();
+    var iss = L.marker([latitude, longitude],{icon: IconISS});
+
+    if (exlatitude!=0 && exlongitude!=0) {
+        var positions = [new L.LatLng(exlatitude, exlongitude), new L.LatLng(latitude, longitude)];
+        var trace = new L.Polyline(positions);
+        trace.addTo(traceLayer);
+    }
+
+    if ((document.getElementById("controleMap").checked)) {
+        map.setView([latitude, longitude], 6);
+    }
+
+    iss.addTo(markerLayer);
+    markerLayer.addTo(map);
+    traceLayer.addTo(map);
+}
+
+// Fonction de mise à jour de la carte
+function refreshView() {
+    setTimeout(function() {
+        getCoords();
+        ZoomChange();
+        moveISS();
+        exlatitude = latitude;
+        exlongitude = longitude;
+        refreshView();
+    }, 1000);
+}
